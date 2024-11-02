9c99b9b4e1fe8f1b3d46fd9948d58a10982a4106
tiken14 <tiken14@live.com>
2024-11-02 19:10:21 +0500

    *Добавление полей пользователю (телефон, страна)

--- users/models.py
+++ users/models.py
@@ -4,7 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class MailingUser(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Почта')
-    avatar = models.ImageField(upload_to='users_avatars/', blank=True, null=True)
+    phone_number = models.IntegerField(unique=True, verbose_name="номер телефона")
+    avatar = models.ImageField(upload_to='users_avatars/', blank=True, null=True, verbose_name='фото профиля')
+    country = models.CharField(max_length=20, verbose_name='страна проживания', blank=False, null=False, default='Неизвестно')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
