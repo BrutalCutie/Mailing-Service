c4d66bc51ce51b24895b515b9c7c1df9778e427d
tiken14 <tiken14@live.com>
2024-11-03 09:45:14 +0500

    + Модели получателя(Receiver) и сообщения(Message)

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Receiver(models.Model):
+
+    email = models.EmailField(unique=True, null=False, blank=False, verbose_name="почта получателя")
+    full_name = models.CharField(max_length=100, null=False, blank=False, verbose_name="Ф.И.О.")
+    commentary = models.TextField(verbose_name="Комментарий о получателе рассылки")
+
+    def __str__(self):
+        return f"{self.full_name} | {self.email}"
+
+    class Meta:
+        verbose_name = 'Получатель рассылки'
+        verbose_name_plural = 'Получатели рассылки'
+        # permissions = [()]
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=100, blank=False, null=False, verbose_name="тема сообщения")
+    message_text = models.TextField(verbose_name="текст сообщения")
+
+    def __str__(self):
+        max_length = 20
+        is_long = len(self.subject.__str__()) > max_length
+        add_symb = ''
+        if is_long:
+            add_symb = '...'
+
+        return f"{self.pk} | {self.subject[:max_length]}{add_symb}"
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
