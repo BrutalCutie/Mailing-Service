118253da97ce6aec8538ecc605176b91facfe9a7
tiken14 <tiken14@live.com>
2024-11-05 14:18:20 +0500

    Работа с шаблонами. Добавление favicon

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('mailing.urls')),
-    path('users', include('users.urls')),
+    path('users/', include('users.urls')),
 
     path('admin/', admin.site.urls),
 ]
