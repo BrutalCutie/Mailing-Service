371164a410b24e3df21594417dae70d34d788118
tiken14 <tiken14@live.com>
2024-11-08 16:11:35 +0500

    Отображение получателей

--- mailing/views.py
+++ mailing/views.py
@@ -1,15 +1,18 @@
+import json
+from pprint import pprint
+
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from mailing import forms
-from mailing.models import Mailing
+from mailing.models import Mailing, Receiver, Message
 from users.models import MailingUser
 from mailing.services import MailingService
 
 
-class MainPageView(ListView):
+class MailingListView(ListView):
     model = Mailing
     template_name = 'mailing/main_page.html'
 
@@ -18,8 +21,6 @@ class MainPageView(ListView):
     def get_queryset(self):
         user = self.request.user
         queryset = user.mailings.all()
-
-        print(queryset)
 
         return queryset
 
@@ -51,3 +52,53 @@ class MailingDetailView(DetailView):
         context['receivers_list'] = MailingService.get_sreceivers(mailing_pk)
 
         return context
+
+
+class MailingUpdateView(UpdateView):
+    pass
+
+
+class MailingDeleteView(DeleteView):
+    pass
+
+
+class ReceiverListView(ListView):
+    model = Receiver
+    template_name = 'mailing/receiver_list.html'
+
+    context_object_name = 'receivers'
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = user.receivers.all()
+
+        return queryset
+
+
+class ReceiverCreateView(CreateView):
+    pass
+
+
+class ReceiverDetailView(DetailView):
+    model = Receiver
+    template_name = 'mailing/receiver_detail.html'
+    context_object_name = 'receiver'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['back_page'] = self.request.environ['HTTP_REFERER']
+
+        return context
+
+
+class ReceiverUpdateView(UpdateView):
+    pass
+
+
+class ReceiverDeleteView(DeleteView):
+    pass
+
+
+
+
