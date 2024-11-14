f88a6abc51ad5d7f1e36881e37d675853f8c3254
tiken14 <tiken14@live.com>
2024-11-14 17:28:48 +0500

    Команды add_manager_group и manual_push. Удаление файла с целями

---
+++ users/management/commands/add_manager_group.py
@@ -0,0 +1,25 @@
+from django.core.management import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+
+class Command(BaseCommand):
+    help = """
+    Добавляет группу менеджеров с необходимой настройкой
+    """
+
+    def handle(self, *args, **options):
+        can_manage_users = Permission.objects.get(codename='can_manage_users')
+        can_manage_clients = Permission.objects.get(codename='can_manage_clients')
+        can_manage_message = Permission.objects.get(codename='can_manage_message')
+        can_manage_mailing = Permission.objects.get(codename='can_manage_mailing')
+
+        manager_group = Group(name='manager')
+        manager_group.permissions.add(
+            can_manage_users, can_manage_clients, can_manage_message, can_manage_mailing
+        )
+        manager_group.save()
+
+        print("Группа manager добавлена")
+
+
+
